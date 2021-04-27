376d7888352247c57bd66d3feb5cd3a873057759
Nikita Mullin <vekkby@protonmail.com>
2021-04-27 13:02:27 +0300

    wip_reviewer_link_fix

--- lib/events/link_shared.rb
+++ lib/events/link_shared.rb
@@ -1,5 +1,7 @@
 REDMINE_DOMAIN = ENV['REDMINE_DOMAIN']
 ISSUE_URL = "#{REDMINE_DOMAIN}/issues/"
+USERS_URL = "#{REDMINE_DOMAIN}/users/".freeze
+REVIEWER = 'Ревьюер'.freeze
 
 SlackRubyBotServer::Events.configure do |config|
   config.on :event, 'event_callback', 'link_shared' do |event|
@@ -10,23 +12,41 @@ SlackRubyBotServer::Events.configure do |config|
           issue = Redmine.issue issue_id
           assigned_to = Redmine.user issue[:assigned_to][:id]
 
-          issue[:custom_fields].push(
-            {
-              name: "Tags",
-              value: issue[:journals].map { |elem|
-                elem[:details].filter { |detail|
-                  detail[:name] == "tag_list"
-                }.map { |value|
-                  value[:new_value]
-                }
-              }.filter { |elem| elem.present? }.last
+          tags = issue[:journals].map { |elem|
+            elem[:details].filter { |detail|
+              detail[:name] == "tag_list"
+            }.map { |value|
+              value[:new_value]
             }
-          )
+          }.filter { |elem| elem.present? }.last
+
+          issue[:custom_fields].push({ name: "Tags", value: tags })
+
+
           custom_fields = issue[:custom_fields].filter {
             |elem| elem[:value].present?
           }.map {
             |elem| "#{elem[:name]}: #{elem[:value].kind_of?(Array) ? elem[:value].join(', ') : elem[:value]}"
           }.join "\n"
+
+          custom_fields_array = issue[:custom_fields].each_with_object([]) do |(name, value), acc|
+            next if value.blank?
+
+            field_value = if value.is_a?(Array)
+                            value.join(', ')
+                          elsif  name == REVIEWER
+                            "#{USERS_URL}#{value}" # как пример
+                          else
+                            value
+                          end
+
+            acc << "#{name}: #{field_value}"
+          end
+
+          custom_fields = custom_fields_array.join("\n")
+
+          author_text = "<#{USERS_URL}#{issue[:author][:id]}|#{issue[:author][:name]}>"
+          assignee_text = "<#{USERS_URL}#{issue[:assigned_to][:id]}|#{issue[:assigned_to][:name]}>"
 
           unfurls = {
             link[:url] => {
@@ -43,15 +63,15 @@ SlackRubyBotServer::Events.configure do |config|
                   "text": {
                     "type": "mrkdwn",
                     "text": "Status: #{issue[:status][:name]}\n"\
-                            "Author: <#{REDMINE_DOMAIN}/users/#{issue[:author][:id]}|#{issue[:author][:name]}>\n"\
-                            "Assigned To: <#{REDMINE_DOMAIN}/users/#{issue[:assigned_to][:id]}|#{issue[:assigned_to][:name]}>\n"\
+                            "Author: #{author_text}\n"\
+                            "Assigned To: #{assignee_text}\n"\
                             "Estimated Time: #{issue[:estimated_hours]} h.\n"\
                             "Spend time: #{issue[:spent_hours]} h.\n"\
                             "#{custom_fields}"
                   },
                   "accessory": {
                     "type": "image",
-                    "image_url": gavatar(assigned_to[:mail] || assigned_to[:login] + '@okdesk.ru'),
+                    "image_url": gavatar(assigned_to[:mail] || "#{assigned_to[:login]}@okdesk.ru"),
                     "alt_text": "Assigned to #{issue[:assigned_to][:name]}"
                   }
                 }
