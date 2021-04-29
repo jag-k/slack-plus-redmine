00a209655ddb8b16d4e09087e582a5bc67c0381e
oleg.pozdnyakov <oleg.pozdnyakov@okdesk.ru>
2021-04-29 12:37:53 +0300

    Create fields function

--- lib/helpers/unfurls/creator.rb
+++ lib/helpers/unfurls/creator.rb
@@ -10,26 +10,12 @@ module Unfurls
       issue_id = Parser.new(url).call
       if issue_id.present?
         issue = Redmine::Issue.new(issue_id).call
-        assigned_to = Redmine::User.new(issue[:assigned_to][:id]).call
-        author = Redmine::User.new(issue[:author][:id]).call
-
-        tags = Tags.new(issue).call
-
-        fields_array = [
-          { name: "Author", value: author[:link] },
-          { name: "Assigned To", value: assigned_to[:link] },
-          { name: "Estimated Time", value: "#{issue[:estimated_hours]} h." },
-          { name: "Spend time", value: "#{issue[:spent_hours]} h." },
-          { name: "Tags", value: tags }
-        ]
-        fields_array.concat issue[:custom_fields]
-        fields = Fields.new(fields_array).call
 
         {
           url => {
             "blocks": [
               header(issue),
-              section(fields, assigned_to)
+              section(issue)
             ]
           }
         }.to_json
@@ -49,14 +35,31 @@ module Unfurls
       }
     end
 
-    # @param [Array<Hash>] fields Fields to Markdown
+    # @param [Hash] issue Issue Hash-object
     # @param [Hash] assigned_to Redmine User
-    def section(fields, assigned_to)
+    def fields(issue, assigned_to)
+      author = Redmine::User.new(issue[:author][:id]).call
+      tags = Tags.new(issue).call
+
+      fields_array = [
+        { name: "Author", value: author[:link] },
+        { name: "Assigned To", value: assigned_to[:link] },
+        { name: "Estimated Time", value: "#{issue[:estimated_hours]} h." },
+        { name: "Spend time", value: "#{issue[:spent_hours]} h." },
+        { name: "Tags", value: tags }
+      ]
+      fields_array.concat issue[:custom_fields]
+      Fields.new(fields_array).call
+    end
+
+    # @param [Hash] issue Issue Hash-object
+    def section(issue)
+      assigned_to = Redmine::User.new(issue[:assigned_to][:id]).call
       {
         "type": "section",
         "text": {
           "type": "mrkdwn",
-          "text": fields
+          "text": fields(issue, assigned_to)
         },
         "accessory": {
           "type": "image",
